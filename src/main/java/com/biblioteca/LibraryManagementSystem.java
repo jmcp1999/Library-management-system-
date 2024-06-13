01647b4ea891484dfcd88cb8cb85e3610a44b78b
Jose Manuel Cruz Pacheco <cruzpachecojosemanuel@gmail.com>
2024-06-13 12:55:24 +0200

    Created body and loop

---
+++ src/main/java/com/biblioteca/LibraryManagementSystem.java
@@ -0,0 +1,25 @@
+package com.biblioteca;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class LibraryManagementSystem {
+    public static void main(String[] args) {
+        Library library = new Library();
+        Admin admin = new Admin("Admin");
+
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            System.out.println("Library Management System");
+            System.out.println("1. Add Book");
+            System.out.println("2. Search Book by Title");
+            System.out.println("3. Search Book by Author");
+            System.out.println("4. Search Book by Genre");
+            System.out.println("5. Borrow Book");
+            System.out.println("6. Return Book");
+            System.out.println("7. Exit");
+            System.out.print("Choose an option: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
